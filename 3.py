eca28507c0704854a2be53840978880982031912
maks-march <98648053+maks-march@users.noreply.github.com>
2025-08-01 20:40:03 +0500

    Solve 3

---
+++ 3.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    floor = [1]
+
+    def generate(self, numRows):
+        result = [[1]]
+        for i in range(1, numRows):
+            self.floor = self.buildNextFloor()
+            result.append(self.floor)
+        return result
+
+    def buildNextFloor(self):
+        new_floor = [self.floor[i]+self.floor[i+1] for i in range(0, len(self.floor)-1)]
+        new_floor.insert(0, 1)
+        new_floor.append(1)
+        return new_floor
+
+s = Solution()
+print(s.generate(1))
